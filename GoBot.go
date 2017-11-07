f5e2d073813c675b123c13adff8822d0270b7bb9
KevinPriv <kb215@ga.newcastle.sch.uk>
2017-11-07 18:50:36 +0000

    Initial commit

---
+++ GoBot.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"github.com/bwmarrin/discordgo"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"encoding/json"
+)
+
+type Config struct {
+	Token string
+}
+
+func main() {
+	discord, err := discordgo.New("Bot " + getConfig().Token)
+
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	discord.AddHandler(messageCreate)
+	err = discord.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	discord.Close()
+}
+
+func getConfig() Config {
+	file, _ := os.Open("conf.json")
+	decoder := json.NewDecoder(file)
+	configuration := Config{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	return configuration
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+}
+
